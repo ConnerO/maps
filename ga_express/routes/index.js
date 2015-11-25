cd23f812b2081c169c56e7ebbb8478cee46da405
Conner O'Heeron <conneroheeron@gmail.com>
2015-11-25 17:30:58 -0600

    now the final promises array has lats and lngs for every address

--- ga_express/routes/index.js
+++ ga_express/routes/index.js
@@ -6,7 +6,7 @@ var User = require('../models/user');
 var rest = require('../node_modules/restler');
 var Promise = require('promise');
 var moment = require('moment');
-// var test = "";
+
 addresses = [];
 // coordResults = [];
 
@@ -17,7 +17,9 @@ router.get('/', function (req, res) {
 	rest.get("https://data.austintexas.gov/resource/b4y9-5x39.json?$where=starts_with(crime_type, 'DWI')&$limit=10").on('complete', addCoords);
 	
 	Crime.find({}, function(err, dwidata){
+
   	// console.log('anything',err,dwidata);
+
       res.render('index', {
         title: '@ustin m@ps',
         crime: dwidata
@@ -28,8 +30,9 @@ router.get('/', function (req, res) {
 
 // This function will run after we have gained the information from the API
 function addCoords(data) {
-	var promises = [];
-	console.log("STEP 1: GET THE API INFO AND SEND TO FUNCTION addCoords(data): ",data);
+	promises = [];
+	
+	console.log("STEP 1: GET THE API INFO AND SEND TO FUNCTION addCoords(data) TO BE SORTED: ",data);
 	data.forEach(function(element, index) {
 		if (!element.longitude || !element.latitude) {
 			var obj = {
@@ -46,7 +49,7 @@ function addCoords(data) {
 			promises.push(getCoords(obj));
 		}
 		else {
-			promises.push(formatCoords(index, parseFloat(element.latitude), parseFloat(element.longitude)));
+			promises.push(formatCoords(index, element.crime_type, element.time, element.address+", Austin, Texas", parseFloat(element.latitude), parseFloat(element.longitude), element.date, element.incident_report_number));
 		}
 
 		return element;
@@ -60,9 +63,10 @@ function getCoords(obj) {
 	var address = obj.address
 	return new Promise(function(resolve, reject) {
 		rest.get("http://maps.googleapis.com/maps/api/geocode/json?address="+address+"&sensor=false").on('complete', function(geocodedData) {
-				if (!geocodedData.results || !geocodedData.results.length) reject("No results available; check if you're over query limit");
+				if (!geocodedData.results || !geocodedData.results.length) reject("No results available; check if you're over query limit!!!");
 				
 				resolve(formatCoords(obj.index, obj.crime_type, obj.time, address, geocodedData.results[0].geometry.location.lat, geocodedData.results[0].geometry.location.lng, obj.date, obj.incident_report_number));
+
 				console.log("STEP 3.5, INSIDE getCoords(obj) THESE THINGS ARE ABOUT TO BE SENT TO THE function formatCoords(obj, lat, lng): ",obj.index, obj.crime_type, obj.time, address, geocodedData.results[0].geometry.location.lat, geocodedData.results[0].geometry.location.lng);
 		})
 		console.log("STEP 3, INSIDE getCoords(obj) THESE OBJECTS DONT HAVE LATS AND LNGS: ", obj);
@@ -71,22 +75,18 @@ function getCoords(obj) {
 }
 
 function formatCoords(obj, crm, tme, add, lat, lng, dte, inc) {
-	// test = formatCoords(obj, crm, tme, add, lat, lng, dte, inc);
+	console.log("STEP 4: THESE ADDRESSES SHOULD HAVE LATS AND LNGS: ",promises);
 	return {
 		index: obj,
 		crime_type: crm,
 		time: tme,
 		address: add,
-		coords: {
-			lat: lat,
-			lng: lng
-		},
+		lat: lat,
+		lng: lng,
 		date: dte,
 		incident_report_number: inc
 	};
+
 }
-
-
-// console.log("STEP 4: ", test);
 
 module.exports = router;
