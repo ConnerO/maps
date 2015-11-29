cab539e24c1709eaa57fdf261eeb2f39a4b33d35
Conner O'Heeron <conneroheeron@gmail.com>
2015-11-28 19:30:28 -0600

    the final array of geocoded objects is complete. the date has been formatted correctly as well. I need to save this array to my db

--- ga_express/routes/index.js
+++ ga_express/routes/index.js
@@ -7,6 +7,7 @@ var rest = require('../node_modules/restler');
 var Promise = require('promise');
 var moment = require('moment');
 
+
 addresses = [];
 // coordResults = [];
 
@@ -18,7 +19,7 @@ router.get('/', function (req, res) {
 	
 	Crime.find({}, function(err, dwidata){
 
-  	// console.log('anything',err,dwidata);
+  	console.log('anything',err,dwidata);
 
       res.render('index', {
         title: '@ustin m@ps',
@@ -26,13 +27,23 @@ router.get('/', function (req, res) {
       });
   });
 
+
+  // removeExcess();
+
 });
+
+router.post('/', function(req, res) {
+
+
+});
+
 
 // This function will run after we have gained the information from the API
 function addCoords(data) {
 	promises = [];
+	finalArray = [];
 	
-	console.log("STEP 1: GET THE API INFO AND SEND TO FUNCTION addCoords(data) TO BE SORTED: ",data);
+	// console.log("STEP 1: GET THE API INFO AND SEND TO FUNCTION addCoords(data) TO BE SORTED: ",data);
 	data.forEach(function(element, index) {
 		if (!element.longitude || !element.latitude) {
 			var obj = {
@@ -45,17 +56,25 @@ function addCoords(data) {
 				incident_report_number: element.incident_report_number,
 				index: index
 			};
-			console.log("STEP 2: INSIDE FUNCTION addCoords(data) SEPARATE THE ELEMENTS THAT DON'T HAVE LATS AND LNGS THEN SEND THOSE TO getCoords(obj): ",obj);		
+			// console.log("STEP 2: INSIDE FUNCTION addCoords(data) SEPARATE THE ELEMENTS THAT DON'T HAVE LATS AND LNGS THEN SEND THOSE TO getCoords(obj): ",obj);		
 			promises.push(getCoords(obj));
 		}
 		else {
-			promises.push(formatCoords(index, element.crime_type, element.time, element.address+", Austin, Texas", parseFloat(element.latitude), parseFloat(element.longitude), element.date, element.incident_report_number));
+//////////////////// This is where the array is getting cluttered //
+			promises.push(formatCoords(index, element.crime_type, element.time, element.address+", Austin, Texas", parseFloat(element.latitude), parseFloat(element.longitude), moment(element.date*1000).format("MMM Do YYYY"), element.incident_report_number));
 		}
 
 		return element;
 	});
 	Promise.all(promises).then(function(data) {
-		// console.log("THIS IS THE INFORMATION FROM THE addCoords FUNCTION", data.obj);
+		finalArray = data;
+		console.log("THIS IS THE FINAL ARRAY THAT SHOULD BE POSTED TO MONGO: ",finalArray);
+
+		Crime.save(function(err){
+			if(err) console.log("err: ", err);
+			res.json(finalArray);
+		});
+
 	});
 }
 
@@ -67,15 +86,15 @@ function getCoords(obj) {
 				
 				resolve(formatCoords(obj.index, obj.crime_type, obj.time, address, geocodedData.results[0].geometry.location.lat, geocodedData.results[0].geometry.location.lng, obj.date, obj.incident_report_number));
 
-				console.log("STEP 3.5, INSIDE getCoords(obj) THESE THINGS ARE ABOUT TO BE SENT TO THE function formatCoords(obj, lat, lng): ",obj.index, obj.crime_type, obj.time, address, geocodedData.results[0].geometry.location.lat, geocodedData.results[0].geometry.location.lng);
+				// console.log("STEP 3.5, INSIDE getCoords(obj) THESE THINGS ARE ABOUT TO BE SENT TO THE function formatCoords(obj, lat, lng): ",obj.index, obj.crime_type, obj.time, address, geocodedData.results[0].geometry.location.lat, geocodedData.results[0].geometry.location.lng);
 		})
-		console.log("STEP 3, INSIDE getCoords(obj) THESE OBJECTS DONT HAVE LATS AND LNGS: ", obj);
+		// console.log("STEP 3, INSIDE getCoords(obj) THESE OBJECTS DONT HAVE LATS AND LNGS: ", obj);
 	})
 
 }
 
 function formatCoords(obj, crm, tme, add, lat, lng, dte, inc) {
-	console.log("STEP 4: THESE ADDRESSES SHOULD HAVE LATS AND LNGS: ",promises);
+	// console.log("STEP 4: THESE ADDRESSES SHOULD HAVE LATS AND LNGS: ",promises);
 	return {
 		index: obj,
 		crime_type: crm,
@@ -83,10 +102,19 @@ function formatCoords(obj, crm, tme, add, lat, lng, dte, inc) {
 		address: add,
 		lat: lat,
 		lng: lng,
-		date: dte,
+		date: moment(dte*1000).format("MMM Do YYYY"),
 		incident_report_number: inc
 	};
-
 }
+
+function removeExcess(promises) {
+	if (promises._12) {
+		console.log(promises._12);
+	}
+}
+
+
+
+
 
 module.exports = router;
