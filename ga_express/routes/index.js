ed6a2f83c57ca0bc66693c3260950fffe956f73e
Conner O'Heeron <conneroheeron@gmail.com>
2015-12-09 14:37:50 -0600

    created a process queue that sends 5 addresses to Google every second so we can gain lats and lngs without exceeding Googles limits. Great Success.

--- ga_express/routes/index.js
+++ ga_express/routes/index.js
@@ -11,24 +11,18 @@ var moment = require('moment');
 addresses = [];
 
 router.get('/', function (req, res) {
-	rest.get("https://data.austintexas.gov/resource/b4y9-5x39.json?$where=starts_with(crime_type, 'DWI')&$limit=10").on('complete', addCoords);
+	rest.get("https://data.austintexas.gov/resource/b4y9-5x39.json?$where=starts_with(crime_type, 'DWI')&$limit=60").on('complete', addCoords);
 	
 	Crime.find({}, function(err, dwidata){
-  	console.log('ORINIGAL DB DATA GETTING PULLED IN',err,dwidata);
-
-  		// for (var x=0; x < dwidata.length; x++) {
-	      res.render('index', {
-	        title: '@ustin m@ps',
-	        crime: dwidata
-	      });
-		  // }
+    res.render('index', {
+      title: '@ustin m@ps',
+      crime: dwidata
+    });
   });
 
 });
 
 router.get('/test', function(req, res) {   
-	
-	// console.log("HERE GOES ELEMENT: ", element);
 
 	finalArray.forEach(function(element) {
 			if (element) {
@@ -52,14 +46,22 @@ router.get('/test', function(req, res) {
 	});
 });
 
-
 function addCoords(data) {
-	promises = [];
-	finalArray = [];
-	
-	// console.log("STEP 1: GET THE API INFO AND SEND TO FUNCTION addCoords(data) TO BE SORTED: ",data);
+	// console.log("ORIGINAL DATA COMING IN: ", data.length);
+	var promises = [];
+	var queue = [];
+	var tempArray = [];
+
+	var count = 0;
+	// looping through data we get from Austin API
 	data.forEach(function(element, index) {
 		if (!element.longitude || !element.latitude) {
+			count++;
+
+			// var tempArray = tempArray || [];
+			tempArray = tempArray || [];
+
+			// creating template object for lats and lngs and others
 			var obj = {
 				crime_type: element.crime_type,
 				time: element.time,
@@ -69,8 +71,15 @@ function addCoords(data) {
 				date: element.date,
 				incident_report_number: element.incident_report_number
 			};
-			// console.log("STEP 2: INSIDE FUNCTION addCoords(data) SEPARATE THE ELEMENTS THAT DON'T HAVE LATS AND LNGS THEN SEND THOSE TO getCoords(obj): ",obj);		
-			promises.push(getCoords(obj));
+			// passing objs to tempArray
+			tempArray.push(obj);
+			// conditional, if theres 5 things in tempArray,
+			if (tempArray.length === 5) {
+				// push tempArray into queue
+				queue.push(tempArray);
+				// set tempArray to null
+				tempArray = null;
+			}
 		}
 		else {
 			promises.push(formatCoords(element.crime_type, element.time, element.address+", Austin, Texas", parseFloat(element.latitude), parseFloat(element.longitude), element.date, element.incident_report_number));
@@ -78,39 +87,51 @@ function addCoords(data) {
 
 		return element;
 	});
-	Promise.all(promises).then(function(data) {
-		finalArray = data;
-		console.log(data);
+
+	processQueue(queue, promises, function (promises) {
+		Promise.all(promises).then(function (data) {
+			console.log(data);
+		});
 	});
+}
+
+function processQueue(queue, promises, cb) {
+	// check if queue array is empty, if it is then we're done, return the promises array 
+	if (queue.length <= 0) {
+		cb(promises);
+		return true;
+	}
+
+	// setTimeout for 1 second function around everything
+	setTimeout(function() {
+		// pop off first element(array) of queue array
+		var popOff = queue.shift();
+		// loop through the array of 5 objects
+		for (var x = 0; x < popOff.length; x++) {
+			// send those 5 objects to getCoords
+			promises.push(getCoords(popOff[x]));	
+		}
+		// recall processQueue(queue) to restart timer
+		processQueue(queue, promises, cb);
+	}, 1000);
 }
 
 function getCoords(obj) {
 	var address = obj.address
 	return new Promise(function(resolve, reject) {
-		// for (var x=0; resolve.length; x++) {
-
-		// 	setInterval(function(x){
-				rest.get("http://maps.googleapis.com/maps/api/geocode/json?address="+address+"&sensor=false").on('complete', function(geocodedData) {
-						if (!geocodedData.results || !geocodedData.results.length) reject("No results available; check if you're over query limit!!!");
-						
-						resolve(formatCoords(obj.crime_type, obj.time, address, geocodedData.results[0].geometry.location.lat, geocodedData.results[0].geometry.location.lng, obj.date, obj.incident_report_number));
-
-						// console.log("STEP 3.5, INSIDE getCoords(obj) THESE THINGS ARE ABOUT TO BE SENT TO THE function formatCoords(obj, lat, lng): ",obj.index, obj.crime_type, obj.time, address, geocodedData.results[0].geometry.location.lat, geocodedData.results[0].geometry.location.lng);
-
-				}) // end of rest.get
-
-		// 	},1000);	
-
-		// } //end of for loop
-
-		// console.log("STEP 3, INSIDE getCoords(obj) THESE OBJECTS DONT HAVE LATS AND LNGS: ", obj);
-	})
-
+		rest.get("http://maps.googleapis.com/maps/api/geocode/json?address="+address+"&sensor=false").on('complete', function(geocodedData) {
+			if (!geocodedData.results || !geocodedData.results.length) {
+				reject("No results available; check if you're over query limit!!!");
+			}
+			else {
+				resolve(formatCoords(obj.crime_type, obj.time, address, geocodedData.results[0].geometry.location.lat, geocodedData.results[0].geometry.location.lng, obj.date, obj.incident_report_number));
+			}
+		});
+	});
 }
 
 function formatCoords(crm, tme, add, lat, lng, dte, inc) {
-	// console.log("STEP 4: THESE ADDRESSES SHOULD HAVE LATS AND LNGS: ",promises);
-	return {
+	var res = {
 		crime_type: crm,
 		time: tme,
 		address: add,
@@ -119,6 +140,7 @@ function formatCoords(crm, tme, add, lat, lng, dte, inc) {
 		date: dte,
 		incident_report_number: inc
 	};
+	return res;
 }
 
 module.exports = router;
